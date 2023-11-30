1147c74ae370c144a7c2a41e5d02aae731a740a7
Moisés David Ramón Esteban <masemoyv1@gmail.com>
2023-11-30 12:09:23 -0600

    Vista general del administrador

--- PROYECTO D.A.W/app/Views/Administrador/vistaGeneral.php
+++ PROYECTO D.A.W/app/Views/Administrador/vistaGeneral.php
@@ -1,1 +1,58 @@
 <h1 align="center">Inicio</h1>
+<div class="col-2"></div>
+<div class="card-container" style="position:absolute;left:35%;">
+  <div class="card">
+    <img src="https://www.nmas.com.mx/_next/image/?url=https%3A%2F%2Fstatic-live.nmas.com.mx%2Fnmas-news%2Fstyles%2Fcorte_16_9%2Fcloud-storage%2F2023-10%2Facuario-de-parque-tepeyac-costo-boletos-horario-en-cdmx.jpg%3Fh%3D920929c4%26itok%3DzAhfG02w&w=1920&q=70" style="max-width:400px;" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Áreas</h5>
+      <p class="card-text">Áreas con las que cuenta el acuario</p>
+      <a href="/Administrador/areasTabla" class="btn btn-primary">Ver</a>
+    </div>
+  </div>
+
+  <div class="card">
+    <img src="https://veterinarialapastora.com.uy/wp-content/uploads/2017/10/como-armar-un-acuario-te-damos-algunos-consejos-basicos-para-principiantes-A.jpg" style="max-width:400px;" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Animales</h5>
+      <p class="card-text">Administración de animales, editar datos, eliminar registros, o agregar nuevos animales</p>
+      <a href="/Administrador/animalTabla" class="btn btn-primary">Ver</a>
+    </div>
+  </div>
+
+  <div class="card">
+    <img src="https://puebla-es.acuariomichin.com/wp-content/uploads/2021/11/R4.jpg" style="max-width:400px;"class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Reservaciones</h5>
+      <p class="card-text">Reservaciones hechas por los clientes</p>
+      <a href="/Administrador/reservacionesTabla" class="btn btn-primary">Ver</a>
+    </div>
+  </div>
+
+  <div class="card">
+    <img src="https://media.istockphoto.com/id/993103334/es/vector/personas-dos-persona-simple-vector-icono.jpg?s=170667a&w=0&k=20&c=0vu74A-eq_Yhiph0UdhHI-r7MqNWN0BMDcIwU7IPGuY="  style="max-width:400px;"class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Usuarios</h5>
+      <p class="card-text">Usuarios que están registrados en el sistema</p>
+      <a href="/Administrador/usuariosTabla" class="btn btn-primary">Ver</a>
+    </div>
+  </div>
+
+  <div class="card">
+    <img src="https://s2rio.com.br/wp-content/uploads/2018/03/Aqu%C3%A1rio-Marinho-do-Rio-de-Janeiro-Edvaldo-Reis-e-Hudson-Pontes-1024x683.jpg" style="max-width:400px;"class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Atracciones</h5>
+      <p class="card-text">Atracciones que ofrece el acuario</p>
+      <a href="/Administrador/atraccionesTabla" class="btn btn-primary">Ver</a>
+    </div>
+  </div>
+
+  <div class="card">
+    <img src="https://cdn-icons-png.flaticon.com/512/554/554795.png" style="max-width:400px;"class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Empleados</h5>
+      <p class="card-text">Empleados registrados, encargados de diversas áreas</p>
+      <a href="/Administrador/empleadosTabla" class="btn btn-primary">Ver</a>
+    </div>
+  </div>
+</div>
+<div class="col-2"></div>
